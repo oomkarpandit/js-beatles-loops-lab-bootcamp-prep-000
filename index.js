877856b7e76461810fc83aa7edda08c4e2ac9459
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 19:04:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+function theBeatlesPlay(arrayMusician,arrayInstruments){
+  const array=[]
+  for(let i=0;i<arrayMusician.length;i++){
+    array.push(`${arrayMusician[i]} plays ${arrayInstruments[i]}`)
+    console.log(array[i])
+  }
+return array
+}
+
+//second function
+function johnLennonFacts(factsArray){
+  var i=factsArray.length-1
+  console.log(factsArray[i] + "length" +factsArray.length)
+  const shoutFact=[]
+  var ii=0
+  while (ii<factsArray.length){
+  shoutFact.push(`"${factsArray[i]} !!!"`)
+  i--
+  }
+  return shoutFact
+}
